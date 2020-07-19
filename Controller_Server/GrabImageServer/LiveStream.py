668d595b915d4a62da4b5f7c6d38761996d85983
Jason <yf9000555@gmail.com>
2020-07-20 00:00:00 +0800

    新增Live Stream測試

--- Controller_Server/GrabImageServer/LiveStream.py
+++ Controller_Server/GrabImageServer/LiveStream.py
@@ -1,26 +1,47 @@
+import time
+import datetime
 import cv2
 
+# Cam properties
+fps = 30.
+
+# Create capture
 cap = cv2.VideoCapture(0)
 
-framerate = 25.0
 
-out = cv2.VideoWriter('appsrc ! videoconvert ! '
-                      'x264enc noise-reduction=10000 speed-preset=ultrafast tune=zerolatency ! '
-                      'rtph264pay config-interval=1 pt=96 !'
-                      'tcpserversink host=192.168.1.27 port=5000 sync=false',
-                      0, framerate, (640, 480))
 
-while cap.isOpened():
+frame_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+frame_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+# Set camera properties
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
+
+cap.set(cv2.CAP_PROP_FPS, int(fps))
+
+# Define the gstreamer sink
+gst_str_rtp = "appsrc ! videoconvert ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! rtph264pay ! udpsink host=127.0.0.1 port=5000"
+
+
+# Check if cap is open
+if cap.isOpened() is not True:
+    print ("Cannot open camera. Exiting.")
+    quit()
+
+# Create videowriter as a SHM sink
+out = cv2.VideoWriter(gst_str_rtp, 0, int(fps), (int(frame_width), int(frame_height)), True)
+
+# Loop it
+while True:
+    # Get the frame
     ret, frame = cap.read()
-    if ret:
-
+    # Check
+    if ret is True:
+        # Write to SHM
+        print(datetime.datetime.now(),"write")
         out.write(frame)
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
     else:
-        break
+        print ("Camera error.")
+        time.sleep(10)
 
-# Release everything if job is finished
 cap.release()
-out.release()
